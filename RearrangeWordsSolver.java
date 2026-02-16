e7c27a0dd06fa0a574dfe340e823c7dac24e3ab4
alan mathew <145783304+alanmathewfdo@users.noreply.github.com>
2026-02-16 15:42:37 +0530

    Add files via upload

---
+++ RearrangeWordsSolver.java
@@ -0,0 +1,30 @@
+package thidalx;
+
+public class RearrangeWordsSolver {
+	 public String rearrangeWords(String s) {
+		 s = s.toLowerCase();
+		 String[] strArr = s.split(" ");
+
+		 for (int i = 0; i < strArr.length - 1; i++) {
+		     for (int j = 0; j < strArr.length - i - 1; j++) {
+		         if (strArr[j].length() > strArr[j + 1].length()) {
+		             String temp = strArr[j];
+		             strArr[j] = strArr[j + 1];
+		             strArr[j + 1] = temp;
+		         }
+		     }
+		 }
+
+		 StringBuilder sb = new StringBuilder();
+		 for(String i : strArr) {
+			 sb.append(i+" ");
+		 }
+		 sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
+		 return sb.toString();
+	 }
+	 public static void main(String[] args) {
+		 RearrangeWordsSolver wordSolve = new RearrangeWordsSolver();
+		 String input = "Leetcode makes me happy";
+		 System.out.println(wordSolve.rearrangeWords(input));
+	}
+}
